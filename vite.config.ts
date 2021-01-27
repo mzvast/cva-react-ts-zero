a09b25f96cf2adda405a84645125f666b84b7a5a
mzvast <mzvast@gmail.com>
2021-01-27 19:17:55 +0800

    init project

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import reactRefresh from '@vitejs/plugin-react-refresh';
+import {defineConfig} from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [reactRefresh()],
+    server: {
+        hmr: {
+            host: 'localhost',
+            port: 3000,
+        },
+    },
+});
